92f32193d7ddada9d1b820a4e09ad744879683e0
KenWestgate <ken.westgate@qixol.com>
2016-11-22 16:32:40 +0000

    v3.70.8.1

--- nopCommerce_3.70/Shared Solution Items/SharedAssemblyInfo.cs
+++ nopCommerce_3.70/Shared Solution Items/SharedAssemblyInfo.cs
@@ -22,5 +22,5 @@ using System.Runtime.InteropServices;
 //
 // *** NOTE: Ensure the 'Description.txt' in the Plugin and Widget folders are also updated. ***
 //
-[assembly: AssemblyVersion("3.70.8.0")]
-[assembly: AssemblyFileVersion("3.70.8.0")]
+[assembly: AssemblyVersion("3.70.8.1")]
+[assembly: AssemblyFileVersion("3.70.8.1")]
