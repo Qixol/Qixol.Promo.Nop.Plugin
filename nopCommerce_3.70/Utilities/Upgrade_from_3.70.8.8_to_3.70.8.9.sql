c11cdbc3268282c4fa3a5d10a49dba087bce215d
KenWestgate <ken.westgate@qixol.com>
2017-02-28 21:34:31 +0000

    v3.70.8.9 - updated to use v2.6.0.1 of the integration library

---
+++ nopCommerce_3.70/Utilities/Upgrade_from_3.70.8.8_to_3.70.8.9.sql
@@ -0,0 +1,9 @@
+/*
+ * update script for Qixol Promo Nop Commerce plugins
+ *
+ * from version 3.70.8.8 to 3.70.8.9
+ *
+ */
+
+-- cope with the promotion xml schema change - these rows are repopulated by the "pull" scheduled task
+truncate table PromoPromotion
